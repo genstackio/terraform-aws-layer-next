c2ac9e01a170b919b798bc3c7055d46c760416a1
Olivier Hoareau <oha@greenberets.io>
2021-04-07 23:00:48 +0200

    chore: refactoring

--- main.tf
+++ main.tf
@@ -52,7 +52,8 @@ module "lambda-proxy" {
   name              = local.lambda_proxy_name
   config_file       = "${path.module}/config.js"
   providers         = {
-    aws = aws.us-east-1
+    aws           = aws
+    aws.us-east-1 = aws.us-east-1
   }
 }
 module "lambda-next-edge" {
