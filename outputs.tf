f0cce270bff1ff22b9b1ca06f64df9c4f1ea8eaa
Olivier Hoareau <oha@greenberets.io>
2021-04-07 22:57:10 +0200

    chore: refactoring

--- outputs.tf
+++ outputs.tf
@@ -22,9 +22,6 @@ output "lambda_proxy_arn" {
 output "lambda_proxy_qualified_arn" {
   value = module.lambda-proxy.qualified_arn
 }
-output "lambda_proxy_invoke_arn" {
-  value = module.lambda-proxy.invoke_arn
-}
 output "lambda_proxy_name" {
   value = module.lambda-proxy.name
 }
@@ -54,4 +51,16 @@ output "lambda_api_name" {
 }
 output "lambda_api_role_name" {
   value = var.enable_api ? module.lambda-api[0].role_name : null
+}
+output "lambda_next_edge_arn" {
+  value = var.enable_next_edge ? module.lambda-next-edge[0].arn : null
+}
+output "lambda_next_edge_qualified_arn" {
+  value = var.enable_next_edge ? module.lambda-next-edge[0].qualified_arn : null
+}
+output "lambda_next_edge_name" {
+  value = var.enable_next_edge ? module.lambda-next-edge[0].name : null
+}
+output "lambda_next_edge_role_name" {
+  value = var.enable_next_edge ? module.lambda-next-edge[0].role_name : null
 }
