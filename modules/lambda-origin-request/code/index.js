41b75d3dba14fc2ae69fded48fd0191e8b9e78d5
Olivier Hoareau <oha@greenberets.io>
2021-04-28 00:08:30 +0200

    fix: fix

--- modules/lambda-origin-request/code/index.js
+++ modules/lambda-origin-request/code/index.js
@@ -1,6 +1,7 @@
 const {buildDefaultResponse, processRequest} = require('./utils');
 
 const handler = async event => {
+    console.log({event: JSON.stringify(event, null, 4)});
     const records = ((event || {})['Records'] || []);
     if (!records.length) return buildDefaultResponse();
     const request = ((records[0] || {})['cf'] || {})['request'];
--- modules/lambda-origin-response/code/index.js
+++ modules/lambda-origin-response/code/index.js
@@ -1,6 +1,7 @@
 const {buildDefaultResponse, enrichResponse} = require('./utils');
 
 const handler = async event => {
+    console.log({event: JSON.stringify(event, null, 4)});
     const records = ((event || {})['Records'] || []);
     if (!records.length) return buildDefaultResponse();
     const response = ((records[0] || {})['cf'] || {})['response'];
